23aedf5d5b663decde73958c3595bbd8a607f83a
Yung Dai <yungchidai@gmail.com>
2017-10-22 13:24:20 -0400

    pushing the bridging header and an update of the MatchBuilder.swift file

---
+++ Moot/ViewController/MatchBuilder.swift
@@ -0,0 +1,23 @@
+//
+//  MatchBuilder.swift
+//  Moot
+//
+//  Created by Yung Dai on 2017-10-15.
+//  Copyright © 2017 Colin Moseley. All rights reserved.
+//
+
+import UIKit
+
+protocol MatchBuilderDelegate: class {
+    
+    
+}
+
+class MatchBuilder: NSObject {
+    
+    
+
+    weak var delegate: MatchBuilderDelegate?
+    
+    
+}
